adbcf978902f12c132e7d473909447089bb70fc7
Paul <venard.paul@gmail.com>
2019-09-08 17:47:26 +0200

    create sponsor screen and set up loading img

--- screens/SponsorsScreen.js
+++ screens/SponsorsScreen.js
@@ -1,13 +1,71 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import Sponsor from "../components/Sponsor";
+import axios from "axios";
+import API from '../constants/API';
+
 
 export default class SponsorsScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error :'n',
+            sponsors: [],
+            isLoading: false
+        }
+    }
+
+    componentDidMount() {
+        this.getSponsors();
+    }
+
+    getSponsors() {
+        this.setState({
+            isLoading: true,
+        });
+        axios.get(API.endpoints.sponsors)
+            .then(res => {
+                const sponsors = res.data;
+
+                this.setState({
+                    sponsors: sponsors || [],
+                    isLoading: false,
+                });
+            })
+            .catch(e => {
+                console.log(e);
+                this.setState({
+                    error: e.message,
+                    isLoading: false,
+                })
+            });
+    }
+
     render() {
         return (
-            <View>
+            <ScrollView>
+                <View style={styles.container}>
+                    {this.state.sponsors.map((sponsor, index) => {
+                        const style = (index + 1) === 1 || (index + 1) % 3 ?
+                            {alignItems: 'center'} :
+                            {alignItems: 'center', flexBasis: '100%'};
 
-            </View>
+                        return <Sponsor key={index} style={style} sponsorInfo={sponsor}/>
+                    })}
+                </View>
+            </ScrollView>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        flexWrap: 'wrap',
+        paddingTop: 15,
+        backgroundColor: '#fff',
+    },
+});
