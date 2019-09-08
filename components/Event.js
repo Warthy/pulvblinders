928599b240b387ccdc99cdf80d89fde6e514e76d
Paul <venard.paul@gmail.com>
2019-09-08 18:20:55 +0200

    create event screen

---
+++ components/Event.js
@@ -0,0 +1,146 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, ImageBackground, TouchableOpacity, Modal, Platform, ScrollView} from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons';
+import API from '../constants/API';
+import moment from 'moment';
+import Colors from "../constants/Colors";
+
+
+export default function Event(props) {
+    const [modalVisible, toggleModal] = useState(false);
+    const {startsAt, endsAt, location, title, media, description, date} = props.eventInfo
+        , dateFormat = moment(date).format("DD/MM/YYYY"),
+        [hours, minutes] = (endsAt || startsAt).split(':');
+
+    return (
+        <View style={{...styles.member, ...{opacity: moment(date).hours(hours).minutes(minutes).isAfter() ? 1 : 0.4}}}>
+            <Modal animationType="fade" visible={modalVisible} style={styles.modal}>
+                <ScrollView>
+                    <TouchableOpacity style={{flex: 1, flexDirection: 'row-reverse'}}>
+                        <MaterialIcons name="close" size={20} color={Colors.second}/>
+                    </TouchableOpacity>
+                    <Text style={styles.modalTitle}>{title.toUpperCase()}</Text>
+                    <Text style={styles.modalLocation}>{location}</Text>
+
+
+                    <View style={{marginTop: 40, alignSelf: 'stretch'}}>
+                        <Text style={styles.modalSection}> Description : </Text>
+                        <Text style={{textAlign: 'center'}}>{description}</Text>
+                    </View>
+
+                    <View style={{marginTop: 20, alignSelf: 'stretch'}}>
+                        <Text style={styles.modalSection}> Date : </Text>
+                        <Text style={styles.modalDate}>
+                            {dateFormat} - {endsAt ? startsAt + ' - ' + endsAt : startsAt}
+                        </Text>
+                    </View>
+
+                    <TouchableOpacity onPress={() => toggleModal(!modalVisible)} style={styles.closeButton}>
+                        <Text style={styles.close}>Retour</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </Modal>
+
+            <TouchableOpacity onPress={() => toggleModal(!modalVisible)}>
+                <ImageBackground source={{uri: API.media + media}} style={styles.upperPart}>
+                    <Text style={styles.title}>{title.toUpperCase()}</Text>
+                </ImageBackground>
+                <View style={styles.lowerPart}>
+                    <Text style={styles.location}>
+                        <MaterialIcons name="location-on" size={12} color="#c6c6c6"/>
+                        {location}
+                    </Text>
+                    <Text style={styles.time}>
+                        <MaterialIcons name="access-time" size={12} color="#c6c6c6"/>
+                        {endsAt ? startsAt + ' - ' + endsAt : startsAt}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    member: {
+        height: 125,
+        flex: 1,
+        flexDirection: 'column',
+        margin: 5,
+    },
+    upperPart: {
+        height: 100,
+        width: '100%',
+        elevation: 4,
+        borderColor: '#c6c6c6',
+    },
+    title: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 18,
+        textShadowColor: '#00000f',
+        textShadowOffset: {width: 3, height: 3},
+        textShadowRadius: 3
+    },
+    lowerPart: {
+        paddingHorizontal: 3,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        height: 25,
+        width: '100%',
+        elevation: 4,
+        borderColor: '#c6c6c6',
+        backgroundColor: '#fff',
+    },
+    location: {
+        fontSize: 12,
+        color: "#c6c6c6",
+        textAlign: 'left',
+    },
+    time: {
+        fontSize: 12,
+        color: "#c6c6c6",
+        textAlign: 'right',
+    },
+    modalTitle: {
+        marginVertical: 10,
+        color: "#444",
+        textAlign: 'center',
+        fontFamily: 'clarendon-condensed',
+        fontSize: 20,
+    },
+    modalSection: {
+        textAlign: 'left',
+        fontSize: 15,
+        color: "#444",
+        fontWeight: 'bold',
+    },
+    modalDate: {
+        textAlign: 'center',
+        fontSize: 15,
+        color: "#444",
+    },
+    modalLocation: {
+        marginTop: -13,
+        color: "#444",
+        textAlign: 'center',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+    closeButton: {
+        marginTop: 10,
+        flex: 1,
+        minHeight: 30,
+        justifyContent: 'flex-end',
+    },
+    close: {
+        textAlign: 'center',
+        color: Colors.second,
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        fontFamily: Platform.OS === 'android' ? 'sans-serif-light' : undefined,
+        fontSize: 18,
+    },
+});
--- screens/EventsAssociationsScreen.js
+++ screens/EventsAssociationsScreen.js
@@ -1,11 +1,192 @@
 import React from 'react';
-import {View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import axios from 'axios';
+import Event from '../components/Event';
+import API from '../constants/API';
+import moment from 'moment';
 
 export default class EventsAssociationsScreen extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            error: 'n',
+            todayIndex: undefined,
+            events: [],
+            isLoading: false,
+            start: 0,
+        };
+    }
+
+    componentWillMount() {
+        this.setState({
+            isLoading: true,
+        });
+        this.getEvents()
+    }
+
+    componentDidMount() {
+        this.clock = setInterval(() => {
+            if (!this.state.isLoading) {
+                this.scroller.scrollTo({x: 0, y: this.state.start});
+                clearInterval(this.clock);
+            }
+        }, 1000);
+    }
+
+    getEvents() {
+        this.setState({
+            isLoading: true,
+        });
+        axios.get(API.endpoints.events.associations)
+            .then(res => {
+                const events = res.data.reduce((events, event) => {
+                    event.startsAt = moment(event.startsAt).format("HH:mm");
+                    event.endsAt = event.endsAt ? moment(event.endsAt).format("HH:mm") : null;
+
+                    const date = moment(event.date).format("YYYYMMDD");
+                    if (!events[date]) {
+                        events[date] = [];
+                    }
+                    events[date].push(event);
+                    return events;
+                }, {});
+
+                let todayIndex, start = 0;
+                const eventArrays = Object.keys(events).map((date, index) => {
+
+                    if (typeof todayIndex === 'undefined') {
+                        if (moment(date, "YYYYMMDD").isAfter(moment(), 'days')) {
+                            todayIndex = index;
+                            events[date].some((event) => {
+                                const [hours, minutes] = (event.endsAt || event.startsAt).split(':');
+                                return moment(event.date).hours(hours).minute(minutes).isAfter(moment(), minutes) ?
+                                    true :
+                                    !(start += 135);
+                            })
+                        } else {
+                            start += 30 + 135 * events[date].length;
+                        }
+
+                    }
+                    return {
+                        date: moment(date, "YYYYMMDD"),
+                        events: events[date]
+                    };
+                });
+                todayIndex = typeof todayIndex === 'undefined' ? eventArrays.length - 1 : todayIndex;
+                this.setState({
+                    start,
+                    todayIndex,
+                    events: eventArrays,
+                    isLoading: false,
+                });
+
+            })
+            .catch(e => {
+                console.log(e);
+                this.setState({
+                    error: e.message,
+                    isLoading: false,
+                })
+            });
+    }
+
+    _onLayout = (e) => {
+        this.setState({
+            width: e.nativeEvent.layout.width,
+            height: e.nativeEvent.layout.height,
+        })
+    };
+
+    _events = () => this.state.events.map((dayEvents, index, events) => {
+        const viewStyle = {
+            ...styles.dateEvents,
+            ...{height: index + 1 === events.length ? this.state.height : undefined}
+        };
+
+        return (
+            <View key={"event-"+index} style={viewStyle}>
+                <View key={"view-date"} style={styles.dateHeader}>
+                    <Text key={"text-1"} style={styles.dayName}>{dayNames[dayEvents.date.format('d')]}</Text>
+                    <Text key={"text-2"} style={styles.dayNumber}>{dayEvents.date.format('DD')}</Text>
+                    <Text key={"text-3"} style={styles.dayMonth}>{monthNames[dayEvents.date.format('M')-1]}</Text>
+                </View>
+                <View key={"view-event"} style={styles.event}>
+                    {dayEvents.events.map((event, index) => (
+                        <Event key={index} eventInfo={event}/>
+                    ))}
+                </View>
+            </View>
+        )
+    });
 
     render() {
         return (
-            <View></View>
+            <ScrollView
+                style={styles.container}
+                onLayout={this._onLayout}
+                contentOffset={{x: 0, y: this.state.start}}
+                ref={(scroller) => {
+                    this.scroller = scroller
+                }}
+            >
+                {this._events()}
+            </ScrollView>
         );
     }
 }
+const monthNames = [
+    "Janvier", "Février", "Mars",
+    "Avril", "Mai", "Juin",
+    "Juillet", "Août", "Septembre",
+    "Octobre", "Novembre", "Decembre"
+];
+
+const dayNames = [
+    "Dimanche", "Lundi", "Mardi",
+    "Mercredi", "Jeudi", "Vendredi",
+    "Samedi"
+];
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 15,
+        backgroundColor: '#fff',
+    },
+    dateEvents: {
+        width: '100%',
+        flexDirection: 'row',
+        marginVertical: 15,
+    },
+    event: {
+        flex: 8,
+        flexDirection: 'column',
+    },
+    dateHeader: {
+        flex: 4,
+    },
+    dayName: {
+        lineHeight: 30,
+        fontSize: 15,
+        color: "#444",
+        fontWeight: 'bold',
+        textAlign: 'center',
+
+    },
+    dayNumber: {
+        lineHeight: 87,
+        fontSize: 90,
+        fontWeight: 'bold',
+        color: "#c6c6c6",
+        textAlign: 'center',
+
+    },
+    dayMonth: {
+        marginTop: -25,
+        fontSize: 25,
+        textAlign: 'center',
+        color: "#c6c6c6",
+
+    }
+});
