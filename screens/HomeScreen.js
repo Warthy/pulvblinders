68b9d19d4ad5a8991da6e6a93a1ea18ff3a65a23
Paul <venard.paul@gmail.com>
2019-09-27 17:44:32 +0200

    last update insh'

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,42 @@ import axios from "axios";
 import API from '../constants/API';
 import * as WebBrowser from "expo-web-browser";
 import Layout from "../constants/Layout";
+import * as Permissions from "expo-permissions";
+import {Notifications} from "expo";
 
+async function registerForPushNotificationsAsync() {
+    const { status: existingStatus } = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+        // Android remote notification permissions are granted during the app
+        // install, so this will only ask on iOS
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+    }
+
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+        return;
+    }
+
+    // Get the token that uniquely identifies this device
+    let token = await Notifications.getExpoPushTokenAsync();
+    // POST the token to your backend server from where you can retrieve it to send push notifications.
+    return fetch(API.endpoints.phone, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            token,
+        }),
+    });
+}
 
 export default class HomeScreen extends React.Component {
 
@@ -13,15 +48,22 @@ export default class HomeScreen extends React.Component {
         this.state = {
             error: 'n',
             sponsors: [],
-            isLoading: false
-        }
+            isLoading: false,
+            notification: null
+        };
     }
 
     componentDidMount() {
-        this.getSponsors();
+        registerForPushNotificationsAsync();
+        this.listener = Notifications.addListener(this._handleNotification);
+        this._getSponsors();
     }
 
-    getSponsors() {
+    _handleNotification = (notification) => {
+        this.setState({notification: notification});
+    };
+
+    _getSponsors() {
         this.setState({isLoading: true});
 
         axios.get(API.endpoints.sponsors)
--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -4,44 +4,11 @@ import {
     View, RefreshControl
 } from 'react-native';
 import { Notifications } from 'expo';
-import * as Permissions from 'expo-permissions'
 import Articles from '../components/Articles';
 import axios from 'axios';
 import API from '../constants/API';
 
-async function registerForPushNotificationsAsync() {
-    const { status: existingStatus } = await Permissions.getAsync(
-        Permissions.NOTIFICATIONS
-    );
-    let finalStatus = existingStatus;
-    // only ask if permissions have not already been determined, because
-    // iOS won't necessarily prompt the user a second time.
-    if (existingStatus !== 'granted') {
-        // Android remote notification permissions are granted during the app
-        // install, so this will only ask on iOS
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        finalStatus = status;
-    }
 
-    // Stop here if the user did not grant permissions
-    if (finalStatus !== 'granted') {
-        return;
-    }
-
-    // Get the token that uniquely identifies this device
-    let token = await Notifications.getExpoPushTokenAsync();
-    // POST the token to your backend server from where you can retrieve it to send push notifications.
-    return fetch(API.endpoints.phone, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            token,
-        }),
-    });
-}
 
 export default class PostScreen extends React.Component {
 
@@ -83,14 +50,10 @@ export default class PostScreen extends React.Component {
     }
 
     componentDidMount() {
-        registerForPushNotificationsAsync();
-        this.listener = Notifications.addListener(this.listen);
+
         this.fetchNews();
     }
 
-    componentWillUnmount() {
-        this.listener && Notifications.removeListener(this.listen)
-    }
 
     render() {
         let articles = this.state.articles;
